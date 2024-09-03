d5df95f2f4eb1c487604a92f12e7f656c4ac7dce
Kumar Sri Sai Kottagundu <66506725+kumarsrisai@users.noreply.github.com>
2024-09-03 17:53:48 +0530

    Updated var.tf

--- var.tf
+++ var.tf
@@ -12,29 +12,29 @@ variable "role" {
 variable "s3_bucket1" {
   description = "Name of the bucket1"
   type        = string
-  default     = "ddsl-raw-dev1"
+  default     = "ddsl-raw-develop"
 }
 
 variable "s3_bucket2" {
   description = "Name of the bucket2"
   type        = string
-  default     = "ddsl-raw-extended-dev1"
+  default     = "ddsl-raw-extended-develop"
 }
 
 variable "s3_bucket3" {
   description = "Name of the bucket3"
   type        = string
-  default     = "ddsl-dq-dev1"
+  default     = "ddsl-dq-develop"
 }
 
 variable "s3_bucket4" {
   description = "Name of the bucket4"
   type        = string
-  default     = "ddsl-processed-dev1"
+  default     = "ddsl-processed-develop"
 }
 
 variable "s3_bucket5" {
   description = "Name of the bucket5"
   type        = string
-  default     = "ddsl-odsl-domain-dev1"
+  default     = "ddsl-odsl-domain-develop"
 }
